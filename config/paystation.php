623c40c3995fb6698dbe8d729b003bed51518ada
TareqJamilSarkar <tareqjamilsarkar@gmail.com>
2026-05-10 14:35:04 +0600

    payment system

---
+++ config/paystation.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'api_url' => env('PAYSTATION_API_URL', 'https://103.134.89.201/initiate-payment'),
+    'merchant_id' => env('PAYSTATION_MERCHANT_ID', '2573-1775021038'),
+    'password' => env('PAYSTATION_PASSWORD', 'poyt32@ft4e6hgc'),
+    'api_host' => 'api.paystation.com.bd',
+];
