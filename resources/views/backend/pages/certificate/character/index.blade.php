56e95fa2db9f7515b40cbc261c2a25d3053f1c3b
TareqJamilSarkar <tareqjamilsarkar@gmail.com>
2026-05-11 10:30:13 +0600

    update

--- resources/views/backend/pages/certificate/character/index.blade.php
+++ resources/views/backend/pages/certificate/character/index.blade.php
@@ -110,7 +110,7 @@
                                     <th>Sl</th>
                                     <th>Photo</th>
                                     <th>Certificate No</th>
-                                    <th>ID & Name</th>
+                                    <th>NID & Name</th>
                                     <th>Address & Mobile</th>
                                     <!-- <th>Quantity</th> -->
                                     <th>Created At</th>
@@ -138,7 +138,7 @@
 
                                     <td>
                                         <span class="citizen-id">
-                                            {{ bnValue($certificate->system_id ?? '') }}
+                                            {{ $certificate->user->people->nid ?? '' }}
                                         </span><br>
                                         {{ $certificate->user->name }}
                                     </td>
