177e79b7a92238e1a9970ac7d95f9b085db6ee40
TareqJamilSarkar <tareqjamilsarkar@gmail.com>
2026-05-17 12:57:15 +0600

    Succession Certificate Edit

--- resources/views/backend/pages/certificate/succession/index.blade.php
+++ resources/views/backend/pages/certificate/succession/index.blade.php
@@ -175,6 +175,12 @@
                                             <i class="fa fa-file-pdf"></i> BN
                                         </a>
 
+                                        @if(edit_permission())
+                                        <a href="{{ route('succession.edit', $certificate->id) }}" class="btn btn-warning btn-sm">
+                                            <i class="fa fa-edit"></i>
+                                        </a>
+                                        @endif
+
                                     </td>
 
                                 </tr>
