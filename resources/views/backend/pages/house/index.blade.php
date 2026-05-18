bd8e8b0721b10588af041b93fa2821e93cdf02f2
TareqJamilSarkar <tareqjamilsarkar@gmail.com>
2026-05-18 16:20:39 +0600

    feat: add custom DataTables filtering and search bars across tax, bridge, house, land, and road index pages

--- resources/views/backend/pages/house/index.blade.php
+++ resources/views/backend/pages/house/index.blade.php
@@ -1,24 +1,28 @@
 @extends('backend.master', ['mainMenu' => 'House', 'subMenu' =>'HouseList'])
 @push('style')
+<style>
+    .table-action {
+        display: flex;
+        gap: 6px;
+    }
+
+    .row.mb-3 input {
+        height: 32px;
+        font-size: 13px;
+    }
+
+    .row.mb-3 select {
+        height: 32px;
+        font-size: 13px;
+    }
+
+    .dataTables_filter {
+        display: none;
+    }
+</style>
 @endpush
 @section('title', 'House List')
 @section('content')
-   <!-- Content Header (Page header) -->
-   <section class="content-header">
-    <div class="container-fluid">
-      <div class="row mb-2">
-        <div class="col-sm-6">
-          <h1>House List</h1>
-        </div>
-        <div class="col-sm-6">
-          <ol class="breadcrumb float-sm-right">
-            <li class="breadcrumb-item"><a href="{{route('house.index')}}">House List</a></li>
-            <li class="breadcrumb-item active">View</li>
-          </ol>
-        </div>
-      </div>
-    </div><!-- /.container-fluid -->
-  </section>
 
     <!-- Main content -->
     <section class="content">
@@ -30,13 +34,14 @@
                     <!-- Horizontal Form -->
                     <div class="card card-info">
                         <div class="card-header">
-                            <div class="row">
+                            <div class="row align-items-center">
                                 <div class="col-md-6 text-left">
-                                    <h3 class="card-title">House List</h3>
+                                    <h3 class="card-title" style="font-size:24px; font-weight: semi-bold;">House Information</h3>
                                 </div>
                                 <div class="col-md-6 text-right">
                                   @if ((is_superadmin() || Auth::user()->institute_id) && create_permission() )
                                     <a href="{{route('house.create')}}" class="btn btn-primary">Create</a>
+                                    <a href="{{route('house.index')}}" class="btn btn-primary">List</a>
                                    @endif
                                 </div>
                             </div>
@@ -44,6 +49,37 @@
                         <!-- /.card-header -->
 
                         <div class="card-body">
+                            <!-- FILTER BAR -->
+                            <div class="row mb-3 align-items-center g-2">
+                                <div class="col-md-1">
+                                    <select id="tableLength" class="form-control form-control-sm">
+                                        <option value="10">10</option>
+                                        <option value="25">25</option>
+                                        <option value="50">50</option>
+                                        <option value="100">100</option>
+                                    </select>
+                                </div>
+
+                                <div class="col-md-2">
+                                    <input type="text" id="search_house_name" class="form-control form-control-sm" placeholder="House Name">
+                                </div>
+
+                                <div class="col-md-2">
+                                    <input type="text" id="search_owner" class="form-control form-control-sm" placeholder="Owner ID/Name">
+                                </div>
+
+                                <div class="col-md-2">
+                                    <input type="text" id="search_village" class="form-control form-control-sm" placeholder="Village / Ward">
+                                </div>
+                                
+                                <div class="col-md-2">
+                                    <input type="text" id="search_block" class="form-control form-control-sm" placeholder="Block/Section">
+                                </div>
+
+                                <div class="col-md-3">
+                                    <input type="text" id="search_global" class="form-control form-control-sm" placeholder="Search">
+                                </div>
+                            </div>
 
                           <div class="table-responsive">
                             <table id="example1" class="table table-bordered table-striped">
@@ -84,14 +120,14 @@
                                       <td>{{ $house->villageArea->en_name ?? $house->block_section ?? '' }}</td>
                                       <td style="width: 10%">
 
-                                        <div class="table-action">
+                                        <div class="table-action justify-content-center">
                                             @if ((is_superadmin() || Auth::user()->institute_id) && view_permission() )
                                             @if(edit_permission())
                                                 <a href="{{ route('house.show', $house->id) }}" title="View" data-toggle="tooltip" class="btn btn-sm btn-info"><i class="fa fa-eye"></i></a>
                                                 <a href="{{ route('house.edit', $house->id) }}" title="Edit" data-toggle="tooltip" class="btn btn-sm btn-primary"><i class="fa fa-edit"></i></a>
                                             @endif
                                             @if(delete_permission())
-                                                <form class="deleteHouse" method="post">
+                                                <form class="deleteHouse" method="post" style="display:inline;">
                                                   @csrf
                                                   @method('Delete')
                                                   <input type="hidden" class="deleteUrl" name="delete_url" value="{{route('house.destroy', $house->id)}}">
@@ -101,7 +137,6 @@
                                             @endif
                                         </div>
                                         
-
 
                                       </td>
                                     </tr>
@@ -131,6 +166,42 @@
 <script>
 
   $(document).ready(function(){
+      
+    let table = $('#example1').DataTable({
+        responsive: true,
+        autoWidth: false,
+        pageLength: 10,
+        lengthChange: false,
+        order: [[0, 'asc']],
+        columnDefs: [
+            { targets: 5, orderable: false }
+        ]
+    });
+
+    $('#search_house_name').keyup(function() {
+        table.column(1).search(this.value).draw();
+    });
+
+    $('#search_owner').keyup(function() {
+        table.column(2).search(this.value).draw();
+    });
+    
+    $('#search_village').keyup(function() {
+        table.column(3).search(this.value).draw();
+    });
+
+    $('#search_block').keyup(function() {
+        table.column(4).search(this.value).draw();
+    });
+
+    $('#search_global').keyup(function() {
+        table.search(this.value).draw();
+    });
+
+    $('#tableLength').change(function() {
+        table.page.len($(this).val()).draw();
+    });
+
     $(".deleteHouse").on('submit', function(e){
       e.preventDefault();
       var thisForm = $(this);
--- resources/views/backend/pages/land/index.blade.php
+++ resources/views/backend/pages/land/index.blade.php
@@ -1,24 +1,28 @@
 @extends('backend.master', ['mainMenu' => 'Land', 'subMenu' =>'LandList'])
 @push('style')
+<style>
+    .table-action {
+        display: flex;
+        gap: 6px;
+    }
+
+    .row.mb-3 input {
+        height: 32px;
+        font-size: 13px;
+    }
+
+    .row.mb-3 select {
+        height: 32px;
+        font-size: 13px;
+    }
+
+    .dataTables_filter {
+        display: none;
+    }
+</style>
 @endpush
 @section('title', 'Land List')
 @section('content')
-   <!-- Content Header (Page header) -->
-   <section class="content-header">
-    <div class="container-fluid">
-      <div class="row mb-2">
-        <div class="col-sm-6">
-          <h1>Land List</h1>
-        </div>
-        <div class="col-sm-6">
-          <ol class="breadcrumb float-sm-right">
-            <li class="breadcrumb-item"><a href="{{route('land.index')}}">Land List</a></li>
-            <li class="breadcrumb-item active">View</li>
-          </ol>
-        </div>
-      </div>
-    </div><!-- /.container-fluid -->
-  </section>
 
     <!-- Main content -->
     <section class="content">
@@ -30,18 +34,51 @@
                     <!-- Horizontal Form -->
                     <div class="card card-info">
                         <div class="card-header">
-                            <div class="row">
+                            <div class="row align-items-center">
                                 <div class="col-md-6 text-left">
-                                    <h3 class="card-title">Land List</h3>
+                                    <h3 class="card-title" style="font-size:24px; font-weight: semi-bold;">Land Information</h3>
                                 </div>
                                 <div class="col-md-6 text-right">
                                     <a href="{{route('land.create')}}" class="btn btn-primary">Create</a>
+                                    <a href="{{route('land.index')}}" class="btn btn-primary">List</a>
                                 </div>
                             </div>
                         </div>
                         <!-- /.card-header -->
 
                         <div class="card-body">
+                            <!-- FILTER BAR -->
+                            <div class="row mb-3 align-items-center g-2">
+                                <div class="col-md-1">
+                                    <select id="tableLength" class="form-control form-control-sm">
+                                        <option value="10">10</option>
+                                        <option value="25">25</option>
+                                        <option value="50">50</option>
+                                        <option value="100">100</option>
+                                    </select>
+                                </div>
+
+                                <div class="col-md-2">
+                                    <input type="text" id="search_dag" class="form-control form-control-sm" placeholder="Dag No.">
+                                </div>
+
+                                <div class="col-md-2">
+                                    <input type="text" id="search_khotian" class="form-control form-control-sm" placeholder="Khotian No.">
+                                </div>
+
+                                <div class="col-md-2">
+                                    <input type="text" id="search_mouza" class="form-control form-control-sm" placeholder="Mouza & Thana">
+                                </div>
+
+                                <div class="col-md-2">
+                                    <input type="text" id="search_owner" class="form-control form-control-sm" placeholder="Owner ID/Name">
+                                </div>
+
+                                <div class="col-md-3">
+                                    <input type="text" id="search_global" class="form-control form-control-sm" placeholder="Search">
+                                </div>
+                            </div>
+
                             <table id="example1" class="table table-bordered table-striped">
                               <thead>
                                 <tr>
@@ -54,6 +91,7 @@
                                     <th>Action</th>
                                 </tr>
                               </thead>
+                              <tbody>
                                 @foreach($lands as $key => $land)
                                     @php
                                         $records = $land->records_data ?? [];
@@ -71,18 +109,19 @@
                                         <td class="text-center">{{ $districtName }}</td>
                                         <td class="text-center">{{ $land->owner_id }} - {{ $ownerName }}</td>
                                         <td class="text-center">
-                                            <a href="{{ route('land.show', $land->id) }}" class="btn btn-sm btn-info" title="View"><i class="fas fa-eye"></i></a>
-                                            <a href="{{ route('land.edit', $land->id) }}" class="btn btn-sm btn-primary" title="Edit"><i class="fas fa-edit"></i></a>
-                                            <form action="{{ route('land.destroy', $land->id) }}" method="POST" style="display:inline;" onsubmit="return confirm('Are you sure you want to delete this record?');">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit" class="btn btn-sm btn-danger" title="Delete"><i class="fas fa-trash"></i></button>
-                                            </form>
+                                            <div class="table-action justify-content-center">
+                                                <a href="{{ route('land.show', $land->id) }}" class="btn btn-sm btn-info" title="View"><i class="fas fa-eye"></i></a>
+                                                <a href="{{ route('land.edit', $land->id) }}" class="btn btn-sm btn-primary" title="Edit"><i class="fas fa-edit"></i></a>
+                                                <form action="{{ route('land.destroy', $land->id) }}" method="POST" style="display:inline;" onsubmit="return confirm('Are you sure you want to delete this record?');">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="btn btn-sm btn-danger" title="Delete"><i class="fas fa-trash"></i></button>
+                                                </form>
+                                            </div>
                                         </td>
                                     </tr>
                                 @endforeach
                               </tbody>
-
                             </table>
                           </div>
                           <!-- /.card-body -->
@@ -98,5 +137,43 @@
 
 @endsection
 @push('script')
+<script>
+    $(function() {
+        let table = $('#example1').DataTable({
+            responsive: true,
+            autoWidth: false,
+            pageLength: 10,
+            lengthChange: false,
+            order: [[0, 'asc']],
+            columnDefs: [
+                { targets: 6, orderable: false }
+            ]
+        });
+
+        $('#search_dag').keyup(function() {
+            table.column(1).search(this.value).draw();
+        });
+
+        $('#search_khotian').keyup(function() {
+            table.column(2).search(this.value).draw();
+        });
+        
+        $('#search_mouza').keyup(function() {
+            table.column(3).search(this.value).draw();
+        });
+
+        $('#search_owner').keyup(function() {
+            table.column(5).search(this.value).draw();
+        });
+
+        $('#search_global').keyup(function() {
+            table.search(this.value).draw();
+        });
+
+        $('#tableLength').change(function() {
+            table.page.len($(this).val()).draw();
+        });
+    });
+</script>
 @endpush
 
